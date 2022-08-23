0302557b16e82abb28a71763b4fe0b280741a916
danyelmnr <danyel.mendes.nogueira@gmail.com>
2022-08-23 10:52:35 -0300

    Changehosts code
    Changehosts

--- garbage-cleaning.sh
+++ garbage-cleaning.sh
@@ -8,27 +8,27 @@
 echo "Task 1: Update system...\n"
 apt-get update && apt-get upgrade -y
 
-#removendo lixo e pacotes temporários de programas instalados que foram atualizados
+#removendo lixo e pacotes temporarios de programas instalados que foram atualizados
 echo "Task 2: Removing junk and temporary packages...\n"
-du -h /var/cache/apt/archives/ 
-apt-get clean && apt-get autoclean 
-apt-get autoremove apt-get 
+du -h /var/cache/apt/archives/
+apt-get clean && apt-get autoclean
+apt-get autoremove apt-get
 
 #Limpando a Lixeira do linux
 echo "Task 3: Removing junk and temporary packages...\n"
 rm -Rf ~/.local/share/Trash/files/*
 
-#limpando os kernels que não são mais utilizados e que continuam no seu hard disk
+#limpando os kernels que nï¿½o sï¿½o mais utilizados e que continuam no seu hard disk
 echo "Task 4: Cleaning the kernels that are no longer used...\n"
 dpkg -l 'linux-*' | sed '/^ii/!d;/'"$(uname -r | sed "s/\(.*\)-\([^0-9]\+\)/\1/")"'/d;s/^[^ ]* [^ ]* \([^ ]*\).*/\1/;/[0-9]/!d' | xargs apt-get -y purge
 
 #limpando os caches
 echo "Task 5: Cleaning caches...\n"
-echo 3 > /proc/sys/vm/drop_caches
+echo 3 >/proc/sys/vm/drop_caches
 sysctl -w vm.drop_caches=3
-#faz com que todo o cache do sistema de aquivos que está temporariamente armazenado na memória cache,
+#faz com que todo o cache do sistema de aquivos que estï¿½ temporariamente armazenado na memï¿½ria cache,
 #seja despejado em disco e liberado, prevenindo assim que se tenha perda de dados.
-sync;
-echo 1 > /proc/sys/vm/drop_caches;
-#Liberar pagecache, inodes e dentries: 
-echo 3 > /proc/sys/vm/drop_caches
+sync
+echo 1 >/proc/sys/vm/drop_caches
+#Liberar pagecache, inodes e dentries:
+echo 3 >/proc/sys/vm/drop_caches
--- hardening-base.sh
+++ hardening-base.sh
@@ -25,32 +25,32 @@ ufw allow 53/udp
 ufw enable
 
 # limite de sessão para usuários do sistema
-echo "export TMOUT=300" >> /etc/profile
-echo "export TMOUT=300" >> /root/.bashrc
-echo "export TMOUT=300" >> /etc/skel/.bashrc
+echo "export TMOUT=300" >>/etc/profile
+echo "export TMOUT=300" >>/root/.bashrc
+echo "export TMOUT=300" >>/etc/skel/.bashrc
 
 # CONFIGURAÇÕES SSH
 # realiza backup de conf do arquivo sshd_config
 cp /etc/ssh/sshd_config /etc/ssh/sshd_config.old
 # Definindo porta alta para o SSH (Port 4107)
-sed -i 's/^#\(Port\).*/\1 4107/' /etc/ssh/sshd_config;
-echo 'Protocol 2' >> /etc/ssh/sshd_config
+sed -i 's/^#\(Port\).*/\1 4107/' /etc/ssh/sshd_config
+echo 'Protocol 2' >>/etc/ssh/sshd_config
 
 # ativação de Banner em sessão SSH
 banner_path='/etc/issue.net'
-sed -i 's/^#\(Banner\).*/\1 ${banner_path}/' /etc/ssh/sshd_config;
+sed -i 's/^#\(Banner\).*/\1 ${banner_path}/' /etc/ssh/sshd_config
 # Configuração do Banner do sistema
-echo -e '\nWelcome! \n\nSession started successfully. Everything done in the system will be logged. \n\nContact: danyel.mendes@ifsertao-pe.edu.br\n' > /etc/issue.net
+echo -e '\nWelcome! \n\nSession started successfully. Everything done in the system will be logged. \n\nContact: danyel.mendes@ifsertao-pe.edu.br\n' >/etc/issue.net
 
 # ativação de log de SSH
-sed -i 's/^#\(SyslogFacility\).*/\1 AUTH/' /etc/ssh/sshd_config;
-sed -i 's/^#\(LogLevel\).*/\1 INFO/' /etc/ssh/sshd_config;
+sed -i 's/^#\(SyslogFacility\).*/\1 AUTH/' /etc/ssh/sshd_config
+sed -i 's/^#\(LogLevel\).*/\1 INFO/' /etc/ssh/sshd_config
 # configuração de parâmetros importantes para segurança do SSH
-sed -i 's/^#\(PermitRootLogin\).*/\1 no/' /etc/ssh/sshd_config;
+sed -i 's/^#\(PermitRootLogin\).*/\1 no/' /etc/ssh/sshd_config
 # Define apenas 2 seções SSH
-sed -i 's/^#\(MaxSessions\).*/\1 2/' /etc/ssh/sshd_config;
+sed -i 's/^#\(MaxSessions\).*/\1 2/' /etc/ssh/sshd_config
 # Proibe senhas em branco
-sed -i 's/^#\(PermitEmptyPasswords\).*/\1 no/' /etc/ssh/sshd_config;
+sed -i 's/^#\(PermitEmptyPasswords\).*/\1 no/' /etc/ssh/sshd_config
 
 # Desativando o login via senha, deixando apenas via chaves assimétricas (Configure a sua chave publica antes nos clientes)
 sed -i 's/^#\(PasswordAuthentication\).*/\1 no/' /etc/ssh/sshd_config
@@ -90,8 +90,8 @@ service chrony restart
 
 # ash colorido para usuários
 mkdir /etc/skel/.bashrc
-echo "export LS_OPTIONS='--color=auto'" >> /etc/skel/.bashrc
-echo "PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u\[\033[01;34m\]@\[\033[01;31m\]\h\[\033[00m\]:\[\033[01;37m\]\w\[\033[01;32m\]\$\[\033[00m\] '" >> /etc/skel/.bashrc
+echo "export LS_OPTIONS='--color=auto'" >>/etc/skel/.bashrc
+echo "PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u\[\033[01;34m\]@\[\033[01;31m\]\h\[\033[00m\]:\[\033[01;37m\]\w\[\033[01;32m\]\$\[\033[00m\] '" >>/etc/skel/.bashrc
 
 # criação de pasta de diretório de  scripts
 cd /
